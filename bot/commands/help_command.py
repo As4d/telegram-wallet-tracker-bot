01cd9d4f11936d8a898f13695dcc87c9b3cba193
Asad <asadkhan91003@gmail.com>
2025-01-13 05:05:26 +0000

    Update help_command to include context parameter as it is mandatory

--- bot/commands/help_command.py
+++ bot/commands/help_command.py
@@ -7,8 +7,9 @@ Functions:
 
 from pathlib import Path
 from telegram import Update
+from telegram.ext import ContextTypes
 
-async def help_command(update: Update):
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """Handles the /help command."""
     message_path = Path(__file__).parent.parent / "messages" / "help.md"
     with open(message_path, "r", encoding="utf-8") as f:
